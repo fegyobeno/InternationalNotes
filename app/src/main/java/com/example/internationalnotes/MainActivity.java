f1601897f8bca1b97379a3679a83a0bdd7dcac60
et1141 <adrian.walczak119@gmail.com>
2023-10-26 16:16:36 +0100

    Database is working more less

--- app/src/main/java/com/example/internationalnotes/MainActivity.java
+++ app/src/main/java/com/example/internationalnotes/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.internationalnotes;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.content.ContentValues;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.os.Bundle;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myTextView = findViewById(R.id.myTextView); // Odnajduje TextView po identyfikatorze
+
+
+        usersOpenHelper users_helper = new usersOpenHelper(this);
+        SQLiteDatabase db = users_helper.getWritableDatabase();
+
+        addUser(db, "Adrian", "admin", 2);
+        addUser(db, "nasa42", "user", 1);
+        addUser(db, "mathias420", "user", 1);
+
+        Cursor cursor = db.rawQuery("SELECT * FROM Users WHERE user = 'Adrian'", null);
+
+        if (cursor.moveToFirst()) {
+            String password = cursor.getString(cursor.getColumnIndex("password"));
+            myTextView.setText(password);
+        }
+        db.close();
+    }
+
+    private void addUser(SQLiteDatabase db, String username, String password, int permission) {
+        ContentValues values = new ContentValues();
+        values.put("user", username);
+        values.put("password", password);
+        values.put("permission", permission);
+
+        db.insert("Users", null, values);
     }
 }
